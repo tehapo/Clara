e02228dca0ec0e1addd181db20221e20722bd5d5
Teemu Pöntelin <tehapo@gmail.com>
2012-08-22 23:56:26 +0300

    Added support for a standard directory for the layout files (VAADIN/layouts).

--- clara/src/main/java/org/vaadin/teemu/clara/LayoutInflater.java
+++ clara/src/main/java/org/vaadin/teemu/clara/LayoutInflater.java
@@ -1,5 +1,8 @@
 package org.vaadin.teemu.clara;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -11,12 +14,39 @@ import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
 
+import com.vaadin.Application;
+import com.vaadin.service.ApplicationContext;
+
 public class LayoutInflater {
 
     private ComponentManager componentManager = new DefaultComponentManager();
 
     public void setComponentManager(ComponentManager componentManager) {
         this.componentManager = componentManager;
+    }
+
+    /**
+     * Inflates the XML layout file from {@code VAADIN/layouts} directory. Some
+     * examples of valid {@code xmlFile} parameter values include
+     * {@code mylayout.xml} or {@code mylayouts/mylayout.xml}. <br />
+     * <br />
+     * <b>This method has not been yet tested within portal environment.</b>
+     * 
+     * @param app
+     * @param xmlFile
+     * @return
+     * @throws LayoutInflaterException
+     */
+    // TODO Test in portal environment.
+    public InflatedCustomComponent inflate(Application app, String xmlFile)
+            throws LayoutInflaterException {
+        File layoutFile = getLayoutFile(app.getContext(), xmlFile);
+        try {
+            return inflate(new FileInputStream(layoutFile));
+        } catch (FileNotFoundException e) {
+            throw new LayoutInflaterException("Given file "
+                    + layoutFile.getAbsolutePath() + " does not exist.");
+        }
     }
 
     public InflatedCustomComponent inflate(InputStream xml)
@@ -51,4 +81,10 @@ public class LayoutInflater {
         componentManager.removeInterceptor(attributeInterceptor);
     }
 
+    private static File getLayoutFile(ApplicationContext context,
+            String filepath) {
+        return new File(context.getBaseDirectory().getAbsoluteFile()
+                + File.separator + "VAADIN" + File.separator + "layouts"
+                + File.separator + filepath);
+    }
 }
