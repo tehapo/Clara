1f213e6878dc5e1228e79b3d5ae2cf29671b7cbb
Teemu Pöntelin <tehapo@gmail.com>
2012-08-24 14:47:29 +0300

    Added Javadocs for the binder annotation types.

--- clara/src/main/java/org/vaadin/teemu/clara/binder/annotation/DataSource.java
+++ clara/src/main/java/org/vaadin/teemu/clara/binder/annotation/DataSource.java
@@ -5,9 +5,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.vaadin.data.Container;
+import com.vaadin.data.Item;
+import com.vaadin.data.Property;
+
 /**
- * Indicates that a method decorated with this annotation is a data source for a
- * UI component.
+ * Indicates that the return value of a method decorated with this annotation
+ * can be used as a data source for UI components.
+ * 
+ * <br />
+ * <br />
+ * This means that the return value must implement one of the Vaadin data model
+ * interfaces ({@link Property}, {@link Item} or {@link Container}).
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
--- clara/src/main/java/org/vaadin/teemu/clara/binder/annotation/EventHandler.java
+++ clara/src/main/java/org/vaadin/teemu/clara/binder/annotation/EventHandler.java
@@ -5,6 +5,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+
+/**
+ * Indicates that a method decorated with this annotation can act as an event
+ * handler for some UI components.
+ * 
+ * <br />
+ * <br />
+ * The type of events that the method will be invoked on is deduced from the
+ * parameter type. For example a method with {@link ClickEvent} parameter can
+ * act as a click listener for a {@link Button}.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface EventHandler {
